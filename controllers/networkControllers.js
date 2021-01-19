b5f81f8c9ae23adfcfd92796c7378faf06896fae
ponlawat-w <ponlawat_w@cmu.ac.th>
2021-01-19 16:06:29 +0100

    Add Network Default Config API

--- controllers/networkControllers.js
+++ controllers/networkControllers.js
@@ -1,4 +1,17 @@
 module.exports = api => {
+  api.get('/network/default-config', (req, res) => {
+    try {
+      const config = JSON.parse(JSON.stringify(api.apiConfig));
+      config.blockchainNetwork.config.host = '127.0.0.1';
+      config.blockchainNetwork.config.port = 8545;
+      config.blockchainNetwork.config.network_id = 2564;
+      config.blockchainNetwork.config.address = '';
+      api.makeResponse.success(res, config);
+    } catch (error) {
+      api.makeResponse.fail(res, 500, 'ERROR', error);
+    }
+  });
+
   api.get('/network/genesis', (req, res) => {
     if (api.apiConfig.blockchainNetwork && api.apiConfig.blockchainNetwork.genesis) {
       api.makeResponse.success(res, api.apiConfig.blockchainNetwork.genesis);
